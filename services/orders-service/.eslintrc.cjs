8c03ee803e40440979d3d9445ca268c5bd062937
Bencho <ruben.camilo321@gmail.com>
2026-08-27 08:55:02 -0500

    feat: migrate services to TypeScript ESLint and update linting configuration

--- services/orders-service/.eslintrc.cjs
+++ services/orders-service/.eslintrc.cjs
@@ -6,14 +6,25 @@ module.exports = {
         es2022: true,
         jest: true,
     },
-    extends: ['eslint:recommended'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 2022,
-        sourceType: 'script',
+        sourceType: 'module',
     },
-    ignorePatterns: ['node_modules/', 'coverage/'],
+    plugins: ['@typescript-eslint'],
+    ignorePatterns: ['node_modules/', 'dist/', 'coverage/'],
     rules: {
-        'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': ['off', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
         'no-console': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-unused-expressions': 'off',
+        'prefer-const': 'off',
+        '@typescript-eslint/no-require-imports': 'off'
     },
 };
